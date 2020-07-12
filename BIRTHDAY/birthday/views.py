98ceda4f3c3e5e7ffce90e60679dd98d25b6f4c4
sampadatalwar <sampadatalwar@gmail.com>
2020-07-12 11:19:24 +0530

    deleting unwanted files

--- BIRTHDAY/birthday/views.py
+++
@@ -1,9 +0,0 @@
-import datetime
-from django.shortcuts import render
-
-# Create your views here.
-def index(request):
-    now = datetime.datetime.now()
-    return render(request, "birthday/isItBirthday.html",{
-        "birthday" : now.month == 8 and now.day == 27       # value of birthday will be true when its 27/08
-    })
--- BIRTHDAY/hello/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.index, name = "index"),
-    path("<str:name>", views.greetUser, name="greet") # custom route that allows us to specify any string in the url 
-]
