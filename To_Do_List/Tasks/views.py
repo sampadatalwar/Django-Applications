13f9587dc5a00475b07e14798caffdb43406388e
sampadatalwar <sampadatalwar@gmail.com>
2020-07-16 23:32:55 +0530

    Added Django forms

--- To_Do_List/Tasks/views.py
+++ To_Do_List/Tasks/views.py
@@ -1,12 +1,35 @@
 from django.shortcuts import render
-#from django.http import HttpResponse
+from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 tasks = ["A", "B", "C"]
+
+
 # Create your views here.
 def index(request):
     return render(request, "Tasks/to-do.html",{
         "tasks": tasks
     })
 
+class AddTaskForm(forms.Form):      # Creating a form class, inheriting from forms.Form
+    newTask = forms.CharField(label= "Enter new task")
+
+
 def addTask(request):
-    return render(request, "Tasks/addTask.html")
+    if request.method  == "POST":
+        form = AddTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["newTask"]     # Forms only get a cleaned_data attribute when is_valid() has been called
+            tasks.append(task)
+            return HttpResponseRedirect(reverse("tasks:index"))
+            """return render(request, "Tasks/to-do.html",{
+            "tasks": tasks
+        })"""
+        else:
+            return render(request, "Tasks/addTask.html",{
+        "form" : form
+        })
+    return render(request, "Tasks/addTask.html",{
+        "form" : AddTaskForm()
+    })
