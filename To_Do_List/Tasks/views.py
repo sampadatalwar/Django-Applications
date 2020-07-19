33fdffe1545917beed3ef4272d134ff73745ed84
sampadatalwar <sampadatalwar@gmail.com>
2020-07-19 23:40:38 +0530

    Sessions in Django

--- To_Do_List/Tasks/views.py
+++ To_Do_List/Tasks/views.py
@@ -3,13 +3,15 @@ from django import forms
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
-tasks = ["A", "B", "C"]
+tasks = []
 
 
 # Create your views here.
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
     return render(request, "Tasks/to-do.html",{
-        "tasks": tasks
+        "tasks": request.session["tasks"]
     })
 
 class AddTaskForm(forms.Form):      # Creating a form class, inheriting from forms.Form
@@ -21,11 +23,10 @@ def addTask(request):
         form = AddTaskForm(request.POST)
         if form.is_valid():
             task = form.cleaned_data["newTask"]     # Forms only get a cleaned_data attribute when is_valid() has been called
-            tasks.append(task)
-            return HttpResponseRedirect(reverse("tasks:index"))
-            """return render(request, "Tasks/to-do.html",{
-            "tasks": tasks
-        })"""
+            request.session["tasks"] += [task]
+            #tasks.append(task)
+            return HttpResponseRedirect(reverse("tasks:index")) # app_name:path name
+            #return render(request, "Tasks/to-do.html",{"tasks": tasks})
         else:
             return render(request, "Tasks/addTask.html",{
         "form" : form
