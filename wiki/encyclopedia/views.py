2505028d6b1bf090766fe86cbc63214c067757d3
sampadatalwar <sampadatalwar@gmail.com>
2020-07-23 17:16:07 +0530

    Converting Markdown to HTML and rendering each title page.

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+import markdown2
+from markdown2 import Markdown
+import markdown2
 
 from . import util
 
@@ -6,5 +9,11 @@ from . import util
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    })
+
+def entryPage(request, title):
+    markdowner=Markdown()
+    return render(request,"encyclopedia/entryPage.html",{
+        "content": markdowner.convert(util.get_entry(title))    # Converting markdown into html format before passing into template.
     })
 
