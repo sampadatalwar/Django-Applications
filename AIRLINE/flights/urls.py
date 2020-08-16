c0a6d15f0efc89455dd37902dca6eb1ea1e93040
sampadatalwar <sampadatalwar@gmail.com>
2020-08-16 14:47:44 +0530

    Add passenger form

--- AIRLINE/flights/urls.py
+++ AIRLINE/flights/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name = "index"),
-    path("<int:flight_id>", views.flight, name = "flight")
+    path("<int:flight_id>", views.flight, name = "flight"),
+    path("<int:flight_id>/bookFlight", views.book_flight, name = "book_flight")
 
 ]
--- AIRLINE/flights/views.py
+++ AIRLINE/flights/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from .models import Flight
+from .models import Flight,Passenger
 
 # Create your views here.
 
@@ -13,5 +13,15 @@ def flight(request, flight_id):
     flight = Flight.objects.get(id = flight_id)
     return render(request, "flights/flight.html", {
         "flight" : flight,
-        "passengers" : flight.passengers.all()
+        "passengers" : flight.passengers.all(),
+        "non_passengers" : Passenger.objects.exclude(flights=flight).all() # Passengers excluding the ones that are already on this flight
     })
+
+def book_flight(request, flight_id):
+    """
+    if request.method == "POST":
+        passenger = Passenger.objects.get(pk=int(request.POST["passenger"]))
+        flight = Flight.objects.get(pk=flight_id)
+    passenger.flights.add(flight)
+    return HttpResponseRedirect(reverse("flight", args=(flight_id,))) 
+    """
