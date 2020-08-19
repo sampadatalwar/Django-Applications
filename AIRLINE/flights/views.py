33cd7a1fd6d189f9a56eabed904fc561c136cffd
sampadatalwar <sampadatalwar@gmail.com>
2020-08-20 00:38:55 +0530

    COMMENTS

--- AIRLINE/flights/views.py
+++ AIRLINE/flights/views.py
@@ -20,9 +20,10 @@ def flight(request, flight_id):
     })
 
 def book_flight(request, flight_id):
+    # For a post request, add a new flight
     if request.method == "POST":
-        passenger = Passenger.objects.get(pk=int(request.POST["passenger"]))
-        flight = Flight.objects.get(pk=flight_id)
-        passenger.flights.add(flight)
+        passenger = Passenger.objects.get(pk=int(request.POST["passenger"]))    # Finding the passenger id from the submitted form data and then finding passenger based on id
+        flight = Flight.objects.get(pk=flight_id)                               #  Accessing the flight
+        passenger.flights.add(flight)                                           # Add passenger to the flight
         return HttpResponseRedirect(reverse("flight", args=(flight_id,))) 
     
