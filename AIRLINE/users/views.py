ab8e12ec239ec9ec184e99fdab97d7dbf9fd1a54
sampadatalwar <sampadatalwar@gmail.com>
2020-08-22 21:50:12 +0530

    Authentication

---
+++ AIRLINE/users/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, "users/index.html")
+
+def login_view(request):
+    return render(request, "users/login.html")
+
+def logout_view(request):
+    return render(request, "users/logout.html")
