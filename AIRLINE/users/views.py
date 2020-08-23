5f93c63e081763305d481b0fed43afbcc05d9974
sampadatalwar <sampadatalwar@gmail.com>
2020-08-23 23:55:43 +0530

    Implementing Authentication

--- AIRLINE/users/views.py
+++ AIRLINE/users/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 
 def index(request):
+    if not request.user.is_authenticated:       # request object has user attribute associated with it, which has is_authenticated attribute.
+                                                    #is_authenticated attribute tells if user is signed in or not.
+        return HttpResponseRedirect(reverse("login_view"))
+
     return render(request, "users/index.html")
 
 def login_view(request):
